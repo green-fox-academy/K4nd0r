28d7a89081391b6ebe23583dfdcc69785aa9c4ec
Andor Korosi <4nd0rkorosi@gmail.com>
2018-10-29 13:04:32 +0100

    java feladatok megcsinálása a COdingHOursig

---
+++ Week-02/untitled/src/com/company/HelloOthers.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        System.out.println("Hello, Esther!"); //Hello, Esther!
+        System.out.println("Hello, Mary!"); //Hello, Mary!
+        System.out.println("Hello, Joe"); //Hello, Joe!
+    }
+}
---
+++ Week-02/untitled/src/com/company/INTOmyself.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class INTOmyself {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        System.out.println("Kőrösi Andor");//  - Your name
+        System.out.println("24");//  - Your age
+        System.out.println("2");//  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+    }
+}
