fd53d49d9875f083dc3bc551589866d770a57a71
Andor Korosi <4nd0rkorosi@gmail.com>
2018-10-31 12:38:44 +0100

    Square

--- Week-02/day-03/src/comp/greenfox/DrawSquare.java
+++ Week-02/day-03/src/comp/greenfox/DrawSquare.java
@@ -4,9 +4,25 @@ import java.util.Scanner;
 
 public class DrawSquare {
     public static void main(String[] args) {
-        int a, b = 1;
-        Scanner line = new Scanner(System.in);
+        System.out.println("Please enter a number!");
+        Scanner scannar = new Scanner(System.in);
+        int number = scannar.nextInt();
 
+        for ( int i = 0; i<number; i++){
+            System.out.print("%");
+        }
+        System.out.println();
+        for ( int i = 0;i<number; i++){
+            System.out.print("%");
+            for ( int k=0; k<number-2;k++){
+                System.out.print(" ");
+            }
+            System.out.print("%");
+            System.out.println(" ");
+        }
+        for (int i =0; i<number; i++){
+            System.out.print("%");
+        }
     }
 }
 // Write a program that reads a number from the standard input, then draws a
