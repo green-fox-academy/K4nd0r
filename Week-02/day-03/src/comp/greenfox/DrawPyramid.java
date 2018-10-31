b69b4a057f69d62c52399a75fc37156ca9600e3b
Andor Korosi <4nd0rkorosi@gmail.com>
2018-10-31 11:23:24 +0100

    pyramid

---
+++ Week-02/day-03/src/comp/greenfox/DrawPyramid.java
@@ -0,0 +1,18 @@
+package comp.greenfox;
+
+public class DrawPyramid {
+    public static void main(String[] args) {
+        for (int i=1; i<10; i += 2)
+        {
+            for (int k=0; k < (4 - i / 2); k++)
+            {
+                System.out.print(" ");
+            }
+            for (int j=0; j<i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
