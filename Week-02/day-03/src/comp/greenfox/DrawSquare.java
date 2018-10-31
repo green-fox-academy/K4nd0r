ef3550c688d091e010a62add53fb174506781956
Andor Korosi <4nd0rkorosi@gmail.com>
2018-10-31 12:06:42 +0100

    diamond half so have to finish

---
+++ Week-02/day-03/src/comp/greenfox/DrawSquare.java
@@ -0,0 +1,23 @@
+package comp.greenfox;
+
+import java.util.Scanner;
+
+public class DrawSquare {
+    public static void main(String[] args) {
+        int a, b = 1;
+        Scanner line = new Scanner(System.in);
+
+    }
+}
+// Write a program that reads a number from the standard input, then draws a
+// square like this:
+//
+//
+// %%%%%%
+// %    %
+// %    %
+// %    %
+// %    %
+// %%%%%%
+//
+// The square should have as many lines as the number was
