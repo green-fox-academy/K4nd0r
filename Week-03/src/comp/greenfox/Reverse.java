fc66254b85ee5ef2b89a5bf7a5c0b6cb487d49da
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-06 14:23:11 +0100

    reverse nice solution

---
+++ Week-03/src/comp/greenfox/Reverse.java
@@ -0,0 +1,17 @@
+package comp.greenfox;
+
+
+
+public class Reverse {
+    public static void main(String[] args) {
+
+        StringBuilder asd = new StringBuilder( ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI" ).reverse();
+
+        // Create a method that can reverse a String, which is passed as the parameter
+        // Use it on this reversed string to check it!
+        // Try to solve this using charAt() first, and optionally anything else after.
+
+        System.out.println(asd);
+    }
+
+}
