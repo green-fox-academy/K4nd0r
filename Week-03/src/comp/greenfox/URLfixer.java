5c60ec6c05f10d4a43eeb0983ac11163fa7fdbc2
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-06 12:19:54 +0100

    change url

---
+++ Week-03/src/comp/greenfox/URLfixer.java
@@ -0,0 +1,15 @@
+package comp.greenfox;
+
+public class URLfixer {
+    public static void main(String[] args) {
+        String url = "https//www.reddit.com/r/nevertellmethebots";
+        String url2 = url.toString().replace("bots", "odds");
+        System.out.println(url2);
+    }
+
+    // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+    // Also, the URL is missing a crutial component, find out what it is and insert it too!
+    // Try to solve it more than once using different String functions!
+
+
+}
