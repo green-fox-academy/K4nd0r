b10df47df3544b6ccbf1f6f571824c99af79ef5f
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-06 15:54:14 +0100

    itlnlist contain

---
+++ Week-03/src/comp/greenfox/IslnList.java
@@ -0,0 +1,27 @@
+package comp.greenfox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IslnList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
+        checkNums(list);
+        // Check if list contains all of the following elements: 4,8,12,16
+        // Create a method that accepts list as an input
+        // it should return "true" if it contains all, otherwise "false"
+
+    }
+
+    public static void checkNums(ArrayList<Integer> isList){
+        boolean contain = false;
+        ArrayList<Integer> temp = new ArrayList<>(Arrays.asList(4, 8, 12, 16));
+        if(isList.containsAll(temp)){
+            contain = true;
+        } else {
+            contain = false;
+        }
+        System.out.println(contain);
+    }
+}
