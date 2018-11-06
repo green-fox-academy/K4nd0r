ef44f9d82a02be2faa724c6bf437125720ae67d2
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-06 13:32:57 +0100

    With StringBuilder

---
+++ Week-03/src/comp/greenfox/TakesLonger.java
@@ -0,0 +1,17 @@
+package comp.greenfox;
+
+
+
+public class TakesLonger {
+    public static void main(String[] args) {
+        StringBuilder quote = new StringBuilder("Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.");
+
+        // When saving this quote a disk error has occured. Please fix it.
+        // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+        // Using pieces of the quote variable (instead of just redefining the string)
+        int indexOfA = quote.indexOf(" you ");  //notice the spaces
+        quote.insert(indexOfA+1, " always takes longer than" );
+
+        System.out.println(quote);
+    }
+}
