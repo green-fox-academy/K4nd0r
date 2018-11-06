35e57840dd9ee31e134a0cba73ee33e0f4e971ce
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-06 16:01:34 +0100

    quote Swap

---
+++ Week-03/src/comp/greenfox/quoteSwaP.java
@@ -0,0 +1,31 @@
+package comp.greenfox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class quoteSwaP {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList("What", "I", "do", "create,", "I", "cannot", "not", "understand."));
+
+        // Accidentally I messed up this quote from Richard Feynman.
+        // Two words are out of place
+        // Your task is to fix it by swapping the right words with code
+        // Create a method called quoteSwap().
+
+        // Also, print the sentence to the output with spaces in between.
+        System.out.println(quoteSwap(list));
+        // Expected output: "What I cannot create I do not understand."
+
+    }
+    public static String quoteSwap(ArrayList<String> correctQuote){
+        Collections.swap(correctQuote, 2, 5);
+
+        String quote = "";
+        for(String yeah : correctQuote) {
+            quote += yeah + " ";
+            //System.out.print(quote);
+        }
+        return quote;
+    }
+}
