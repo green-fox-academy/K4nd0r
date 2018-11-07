cb2a97ea1ef1b05c5f5357590658e349f55faadf
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-07 15:21:34 +0100

    FtotheCenter in every 20px

--- Week-03/Day-03Drawings/src/comp/greenfox/FunctionToCenter.java
+++ Week-03/Day-03Drawings/src/comp/greenfox/FunctionToCenter.java
@@ -4,29 +4,38 @@ import javax.swing.*;
 
 import java.awt.*;
 
-
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-
-
-public class GoToCenter{
-
+public class FunctionToCenter {
+    public static final int canvasSize = 300;
+    public static final int canvasStart = 0;
 
     public static void mainDraw(Graphics graphics) {
         // Create a line drawing function that takes 2 parameters:
         // The x and y coordinates of the line's starting point
         // and draws a line from that point to the center of the canvas.
-        // Draw 3 lines with that function. Use loop for that.
-        for (int i = 0; i < 3; i++) {
-            int xPoint = (int)(Math.random() * (WIDTH + 1));
-            int yPoint = (int)(Math.random() * (HEIGHT + 1));
-            drawLine(graphics, xPoint, yPoint);
+        // Fill the canvas with lines from the edges, every 20 px, to the center.
+        for (int i = 0; i < (canvasSize + 1); i += 20) {
+            drawLine(i, canvasStart, graphics);
+            drawLine(i, canvasSize, graphics);
+            drawLine(canvasStart, i, graphics);
+            drawLine(canvasSize, i, graphics);
         }
     }
+
+    public static void drawLine(int x, int y, Graphics g){
+        g.drawLine(x, y, canvasSize/2, canvasSize/2);
+    }
+    
 
     public  static  void drawLine(Graphics graphics, int startX, int startY){
         graphics.drawLine(startX, startY, WIDTH/2, HEIGHT/2);
     }
+
+
+
+
+
 
     // Don't touch the code below
     static int WIDTH = 320;
