025fed73eacc49c75ea32f6b9905433297740e81
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-08 11:43:11 +0100

    subStrList function search for word parts

--- Week-03/Day-04Pratice/src/comp/greenfox/SubInt.java
+++ Week-03/Day-04Pratice/src/comp/greenfox/SubInt.java
@@ -8,7 +8,7 @@ public class SubInt {
         //  Create a function that takes a number and a list of numbers as a parameter
         //  Returns the indeces of the numbers in the list where the first number is part of
         //  Returns an empty list if the number is not part any of the numbers in the list
-        int a = 1;
+        int a = 0;
         int[] b = { 1,11,34,52,61};
         boolean contains = IntStream.of(b).anyMatch(x -> x == a);
         if ( contains == true){
