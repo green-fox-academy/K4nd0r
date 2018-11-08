51acf2c906d483f051b3f9c2e87746d6f1975450
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-08 16:23:59 +0100

    Golden spiral for starting but not good

---
+++ Week-03/Day-05Pratice/src/comp/greenfox/GoldenRatio.java
@@ -0,0 +1,20 @@
+package comp.greenfox;
+
+public class GoldenRatio {
+    public static void fibonacci(int number) {
+        int fibo1 = 1;
+        int fibo2 = 1;
+        System.out.printf("%nFibonacci series of %d numbers are : ", number);
+        System.out.printf("%s ", fibo1);
+        System.out.printf("%s ", fibo2);
+//        for (int i = 2; i & lt; number ;
+//        i++){
+//            int fibonacci = fibo1 + fibo2;
+//            System.out.printf("%s ", fibonacci);
+//            fibo2 = fibo1;
+//            fibo1 = fibonacci;
+//        }
+    }
+}
+
+
