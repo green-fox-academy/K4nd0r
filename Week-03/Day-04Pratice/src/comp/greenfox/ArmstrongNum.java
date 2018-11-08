533f0440daae8676ffe4285d3a45190aa55a2a31
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-08 14:13:17 +0100

    Armstrong number n digits

---
+++ Week-03/Day-04Pratice/src/comp/greenfox/ArmstrongNum.java
@@ -0,0 +1,30 @@
+package comp.greenfox;
+
+import java.util.Scanner;
+
+public class ArmstrongNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int originalNumber, remainder, result = 0, n = 0;
+
+        originalNumber = number;
+
+        for (;originalNumber != 0; originalNumber /= 10, ++n);
+
+        originalNumber = number;
+
+        for (;originalNumber != 0; originalNumber /= 10)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, n);
+        }
+
+        if(result == number)
+            System.out.println(number + " is an Armstrong number.");
+        else
+            System.out.println(number + " is not an Armstrong number.");
+    }
+}
+
