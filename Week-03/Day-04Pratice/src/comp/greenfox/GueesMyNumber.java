b8175f4dfad5dea533fc1887939e0f89e636bbd7
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-09 16:59:21 +0100

    some changes in order of the demo

--- Week-03/Day-04Pratice/src/comp/greenfox/GueesMyNumber.java
+++ Week-03/Day-04Pratice/src/comp/greenfox/GueesMyNumber.java
@@ -10,36 +10,24 @@ public class GueesMyNumber {
         System.out.println("guess a number between 0-100");
         int lives = 5;
 
-
         for (int i =0 ; i <= 5; i++){
             Scanner scanner = new Scanner(System.in);
             int userGuess = scanner.nextInt();
             if (lives == 0){
                 System.out.println("u lost");
-
-            }
-            else if (userGuess < myNumber) {
+            } else if (userGuess < myNumber) {
                 lives = lives - 1;
                 System.out.println("The stored number is higher! U have " + lives + " lives left.");
                 System.out.println("Guess again!: ");
-
-
 
             } else if (userGuess > myNumber) {
                 lives = lives - 1;
                 System.out.println("The stored number is lower! U have " + lives + " lives left.");
                 System.out.println("Guess again!: ");
 
-
-
             } else if (userGuess == myNumber) {
                 System.out.println("YES");
-
             }
-
         }
-
-
-
 }
 }
