21b24f9a973f3ab956f7070b39b0f803dae8bf9d
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-12 16:43:38 +0100

    logs

---
+++ Week-04/Day-01/src/comp/greenfox/Logs.java
@@ -0,0 +1,59 @@
+package comp.greenfox;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Logs {
+  // Read all data from 'log.txt'.
+  // Each line represents a log message from a web server
+  // Write a function that returns an array with the unique IP addresses.
+  // Write a function that returns the GET / POST request ratio.
+
+  static Path input = Paths.get("log.txt");
+
+  public static void main(String[] args) {
+    getIpAddresses();
+
+    requestRatio();
+  }
+
+  public static String[] getIpAddresses() {
+    try {
+      List<String> tempIpAddresses = Files.readAllLines(input);
+      String[] ipAddresses = new String[tempIpAddresses.size()];
+
+      for (int i = 0; i < ipAddresses.length; i++) {
+        ipAddresses[i] = tempIpAddresses.get(i).substring(27, 38);
+        }
+      return ipAddresses;
+
+    } catch (IOException ie) {
+      System.out.println("Problem.");
+      return new String[0];
+    }
+  }
+
+  public static double requestRatio() {
+    double getCommands = 0;
+    double postCommands = 0;
+
+    try {
+      List<String> tempFileAfterRead = Files.readAllLines(input);
+      for (int i = 0; i < tempFileAfterRead.size(); i++) {
+        if (tempFileAfterRead.get(i) != null) {
+          if (tempFileAfterRead.get(i).contains("GET")) {
+            getCommands++;
+          } else if (tempFileAfterRead.get(i).contains("POST")) {
+            postCommands++;
+          }
+        }
+      }
+      double getPostRatio = (getCommands) * 100 / (getCommands + postCommands);
+      return getPostRatio;
+    } catch (IOException ie) {
+      return 0;
+    }
+  }
+}
