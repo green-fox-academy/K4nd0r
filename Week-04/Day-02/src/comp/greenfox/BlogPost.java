929c96ebbdd818f9a796389fead4c7156e69a6bd
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-13 12:57:03 +0100

    BLogpost with the same main

---
+++ Week-04/Day-02/src/comp/greenfox/BlogPost.java
@@ -0,0 +1,18 @@
+package comp.greenfox;
+
+public class BlogPost {
+  String AuthorName;
+  String Title;
+  String Text;
+  String publicationDate;
+
+  BlogPost(String AuthorName, String Title, String Text, String publicationDate){
+    this.AuthorName = AuthorName;
+    this.Title = Title;
+    this.Text = Text;
+    this.publicationDate = publicationDate;
+
+
+  }
+
+}
--- Week-04/Day-02/src/comp/greenfox/Main.java
+++ Week-04/Day-02/src/comp/greenfox/Main.java
@@ -8,6 +8,27 @@ public class Main {
       PostIt awesome = new PostIt("pink", "Awesome", "black");
       PostIt suberb = new PostIt("yellow", "Superb", "green");
 
+      BlogPost first = new BlogPost(
+          "John Doe",
+          "Lorem Ipsum",
+          "Lorem ipsum dolor sit amet.",
+          "2000.05.04."
+      );
+      BlogPost secound = new BlogPost(
+          "Tim Urban",
+          "Wait but why",
+          "A popular long-form, stick-figure-illustrated blog about almost everything.",
+          "2010.10.10."
+      );
+      BlogPost third = new BlogPost(
+          "William Turton",
+          "One Engineer Is Trying to Get IBM to Reckon With Trump",
+          "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention." +
+              " When I asked to take his picture outside one of IBM’s New York City offices," +
+              " he told me that he wasn’t really into the whole organizer profile thing.",
+          "2017.03.28."
+      );
+
 
     }
 }
