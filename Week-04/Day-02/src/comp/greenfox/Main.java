ef7120591ce2aa46e3170bf969313cb11068437c
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-13 12:45:15 +0100

    1st task

---
+++ Week-04/Day-02/src/comp/greenfox/Main.java
@@ -0,0 +1,13 @@
+package comp.greenfox;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      PostIt idea1 = new PostIt("orange", "Idea 1", "blue");
+      PostIt awesome = new PostIt("pink", "Awesome", "black");
+      PostIt suberb = new PostIt("yellow", "Superb", "green");
+
+
+    }
+}
---
+++ Week-04/Day-02/src/comp/greenfox/PostIt.java
@@ -0,0 +1,13 @@
+package comp.greenfox;
+
+public class PostIt {
+  String setBackground;
+  String text;
+  String setFont;
+
+  PostIt(String setBackground, String text, String setFont) {
+    this.setBackground = setBackground;
+    this.text = text;
+    this.setFont = setFont;
+  }
+}
