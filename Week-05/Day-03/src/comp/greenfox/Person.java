d247378efbf699b171f95716c70e2f501cc3e7a2
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-21 13:30:15 +0100

    person and student are done

--- Week-05/Day-03/src/comp/greenfox/Person.java
+++ Week-05/Day-03/src/comp/greenfox/Person.java
@@ -1,7 +1,19 @@
 package comp.greenfox;
 
 public class Person {
-  String name;
-  int age;
-  String gender;
+  public String name;
+  public int age;
+  public String gender;
+
+  public void introduce(){
+    System.out.println("Hi, I'm " + name +", a " + age +" year old " + gender + ".");
+  }
+  public void getGoal(){
+    System.out.println( "My goal is: Live for the moment!");
+  }
+  Person(){
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+  }
 }
--- Week-05/Day-03/src/comp/greenfox/Student.java
+++ Week-05/Day-03/src/comp/greenfox/Student.java
@@ -1,4 +1,29 @@
 package comp.greenfox;
 
-public class Student {
+public class Student extends Person {
+  String previousOrganization;
+  int skippedDays;
+
+  Student(String name, int age, String gender, String previousOrganization, int skippedDays ){
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+    this.previousOrganization = "The School of Life";
+    this.skippedDays = 0;
+  }
+  public void getGoal(){
+    System.out.println("Be a junior software developer.");
+  }
+  public void introduce(){
+    System.out.println(
+        "Hi, I'm " + name + "," +
+            " a "+ age +" year " + gender + " from " + previousOrganization + " who" +
+            " skipped " + skippedDays + " days from the course already."
+    );
+  }
+  public int skipDays(int numberOfDays){
+    this.skippedDays = skippedDays + numberOfDays;
+    return skippedDays;
+  }
+
 }
