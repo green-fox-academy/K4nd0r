70412b468af8b0b53ea769f57a26562076cbe8f2
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-21 14:02:40 +0100

    sponzot is finished and cohort also

---
+++ Week-05/Day-03/src/comp/greenfox/Cohort.java
@@ -0,0 +1,27 @@
+package comp.greenfox;
+
+import java.util.ArrayList;
+
+
+public class Cohort {
+
+  String name;
+  ArrayList<String> students = new ArrayList<>();
+  ArrayList<String> mentors = new ArrayList<>();
+
+  Cohort(String name) {
+    this.name = name;
+    this.students = new ArrayList();
+    this.mentors = new ArrayList();
+  }
+
+  public void addStudents(Student student){
+    students.add(student.toString());
+  }
+  public void addMentro(Mentor mentor){
+    mentors.add(mentor.toString());
+  }
+  public String info() {
+    return "The " + name  + " cohort has " + students.size() + " students and " + mentors.size() + " mentors.";
+  }
+}
--- Week-05/Day-03/src/comp/greenfox/Sponzor.java
+++ Week-05/Day-03/src/comp/greenfox/Sponzor.java
@@ -4,6 +4,14 @@ public class Sponzor extends Person{
   String company;
   int hiredStudents;
 
+  Sponzor(String name, int age, String gender, String company, int hiredStudents){
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+    this.company = "Google";
+    this.hiredStudents = 0;
+  }
+
   public void introduce(){
     System.out.println(
         "Hi, I'm " + name + " , a " + age + " year old " +
