ee0a3b0c2e64b96e58b75c09613108ba957b5f80
Andor Korosi <4nd0rkorosi@gmail.com>
2018-11-22 17:46:50 +0100

    testing

--- Week-04/Day-05/src/comp/greenfox/FractalTree2own.java
+++ Week-04/Day-05/src/comp/greenfox/FractalTree2own.java
@@ -4,31 +4,57 @@ import javax.swing.*;
 
 import java.awt.*;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class FractalTree2own {
+
+  static Timer timer = new Timer();
+  static int seconds = 100;
+
+
   public static void mainDraw(Graphics g) {
 
-    drawTree(g, WIDTH/2, HEIGHT + 10, -90, 13);
+    drawTree(g, WIDTH/2, HEIGHT - 80, -90, 9);
+
   }
+
   private static void drawTree(Graphics g, int x1, int y1, double angle, int depth) {
+
+
+    TimerTask task;
+    task = new TimerTask() {
+      private final int MAX_SECONDS = 1000;
+
+      @Override
+      public void run() {
+        if (seconds < MAX_SECONDS) {
+          System.out.println("Seconds = " + seconds);
+          seconds = seconds + seconds;
+        } else {
+          // stop the timer
+          cancel();
+        }
+      }
+    };
     if (depth == 0) return;
     int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * depth * 10.0);
     int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * depth * 10.0);
-    g.setColor(new Color(randomRgbColor(), 250, randomRgbColor()));
+    g.setColor(new Color(randomRgbColor(), randomRgbColor(), randomRgbColor()));
     g.drawLine(x1, y1, x2, y2);
     drawTree(g, x2, y2, angle - 20, depth - 1);
     drawTree(g, x2, y2, angle + 20, depth - 1);
-//    Thread.sleep(100);
   }
 
   // Don't touch the code below
-  static int WIDTH = 1920;
-  static int HEIGHT = 1080;
+  static int WIDTH = 1280;
+  static int HEIGHT = 720;
 
   public static void main(String[] args) {
-    JFrame jFrame = new JFrame("Drawing");
+    JFrame jFrame = new JFrame("FractalTree");
     jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     ImagePanel panel = new ImagePanel();
     panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -36,6 +62,7 @@ public class FractalTree2own {
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
     jFrame.pack();
+
 
   }
 
