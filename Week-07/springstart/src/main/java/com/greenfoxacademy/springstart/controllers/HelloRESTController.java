7981d0805f53d9e53e3109e244d1c23add9fc2cc
Andor Korosi <4nd0rkorosi@gmail.com>
2018-12-05 15:18:26 +0100

    world web app

---
+++ Week-07/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloRESTController {
+
+  @RequestMapping(value = "/greeting")
+  @ResponseBody
+  public Greeting greeting(@RequestParam() String name) {
+    Greeting greet = new Greeting(2, "Hello, " + name);
+    return greet;
+  }
+}
