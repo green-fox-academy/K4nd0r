fb7318114cb4bd5fc364fa6c626a4e311a009386
Andor Korosi <4nd0rkorosi@gmail.com>
2018-12-20 09:41:47 +0100

    refactor and add an extra class for players

--- Week-09/Day-03/drinkgame/src/main/java/com/game/drinkgame/drinkservice/DrinkService.java
+++ Week-09/Day-03/drinkgame/src/main/java/com/game/drinkgame/drinkservice/DrinkService.java
@@ -3,5 +3,6 @@ package com.game.drinkgame.drinkservice;
 import org.springframework.stereotype.Service;
 
 @Service
-public class drinkService {
+public class DrinkService {
+
 }
---
+++ Week-09/Day-03/drinkgame/src/main/java/com/game/drinkgame/drinkservice/PlayerService.java
@@ -0,0 +1,8 @@
+package com.game.drinkgame.drinkservice;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlayerService {
+}
