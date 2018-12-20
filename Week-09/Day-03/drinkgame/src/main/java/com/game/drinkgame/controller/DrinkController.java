375cf012a75cae5f2b046ec70b15d20b8fb107f7
Andor Korosi <4nd0rkorosi@gmail.com>
2018-12-20 17:12:11 +0100

    new get mapping

--- Week-09/Day-03/drinkgame/src/main/java/com/game/drinkgame/controller/DrinkController.java
+++ Week-09/Day-03/drinkgame/src/main/java/com/game/drinkgame/controller/DrinkController.java
@@ -4,6 +4,8 @@ package com.game.drinkgame.controller;
 import com.game.drinkgame.drinkservice.DrinkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,7 +18,11 @@ public class DrinkController {
   public DrinkController( DrinkService drinkService) {
     this.drinkService = drinkService;
   }
-
+   @GetMapping("/saved")
+  public String saved(Model model) {
+    model.addAttribute(new DrinkService());
+    return "saved";
+   }
 
 
 }
