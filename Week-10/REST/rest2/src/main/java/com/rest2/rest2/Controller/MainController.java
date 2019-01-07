de996c2fcbe2327735644b481876a7e66279c06d
Andor Korosi <4nd0rkorosi@gmail.com>
2019-01-07 17:39:05 +0100

    new project

---
+++ Week-10/REST/rest2/src/main/java/com/rest2/rest2/Controller/MainController.java
@@ -0,0 +1,13 @@
+package com.rest2.rest2.Controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+  @PostMapping("/arrays")
+  public Object arrays(){
+
+  }
+}
